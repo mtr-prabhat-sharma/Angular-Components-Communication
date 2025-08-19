b7b8054f75b4cae04b9e85e3ab55b06b24f9cbd9
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-08-19 11:30:34 +0530

    added CSS and pagination

--- comnponent-communication/src/app/app.component.ts
+++ comnponent-communication/src/app/app.component.ts
@@ -1,25 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosService } from './todos.service';
-
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UpperCasePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent implements OnInit {
   title = 'comnponent-communication';
-
+  todosList: any[] = [];
+  tableHeaders: any[] = [];
+  paginatedData: any[] = [];
+  currentPage: number = 1;
   constructor(private todos: TodosService) {}
 
   ngOnInit(): void {
-    this.todos.getTodos().subscribe((res) => {
-      console.log("res", res)
-    })
+    this.todos.getTodos().subscribe((res: any) => {
+      console.log('todos', res);
+      this.todosList = res;
+      this.tableHeaders = this.getTableHeaders(this.todosList);
+      this.paginatedData = this.todosList.slice(0, 20);
+      console.log(this.tableHeaders);
+    });
   }
-  
+
+  getTableHeaders(data: any): string[] {
+    const keySet = new Set<string>();
+    data.forEach((item: any) => {
+      Object.keys(item).forEach((key) => {
+        keySet.add(key);
+      });
+    });
+    return [...keySet];
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginatedData = this.todosList.slice(
+        (this.currentPage - 1) * 20,
+        this.currentPage * 20
+      );
+      console.log('prev', this.paginatedData);
+    }
+  }
+
+  nextPage() {
+    console.log('length', this.todosList.length / 20);
+    if (this.currentPage < this.todosList.length / 20) {
+      this.currentPage++;
+      this.paginatedData = this.todosList.slice(
+        (this.currentPage - 1) * 20,
+        this.currentPage * 20
+      );
+      console.log('next', this.paginatedData);
+    }
+  }
 }
---
+++ comnponent-communication/src/app/todos-status/todos-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-status',
+  imports: [],
+  templateUrl: './todos-status.component.html',
+  styleUrl: './todos-status.component.css'
+})
+export class TodosStatusComponent {
+
+}
